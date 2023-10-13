8e326f80910100fc37a192fee5578f81f51bcd9e
waynewenswag <wayne.wen@swag.live>
2023-10-13 17:44:28 +0800

    folder

---
+++ target/compiled/swag_dbt/models/example/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from `swag-wayne-wen`.`analytics`.`my_first_dbt_model`
+where id is null
+
+
---
+++ target/compiled/swag_dbt/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from `swag-wayne-wen`.`analytics`.`my_second_dbt_model`
+where id is null
+
+
